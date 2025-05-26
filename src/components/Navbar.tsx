c4da4c3585109180129743d75937047c1d80a30b
iwasawarenji954 <iwasawarenji954@gmail.com>
2025-05-26 21:25:45 +0900

    update: Stock Auth 導入

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,16 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { HomeIcon, Sprout } from "lucide-react";
+import { HomeIcon, LogIn, LogInIcon, LogOut, Sprout } from "lucide-react";
 import { ModeToggle } from "./ModeToggle";
+import { stackServerApp } from "@/stack";
+import { getUserDetails } from "@/actions/user.actions";
+import { UserButton } from "@stackframe/stack";
 
-function Navbar() {
+async function Navbar() {
+  const user = await stackServerApp.getUser();
+  const app  = stackServerApp.urls;
+  const userProfile = await getUserDetails(user?.id);
+
   return (
     <nav className="sticky top-0 h-12 w-full border-b bg-background/95 backdrop-blur-xl supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
@@ -19,21 +26,47 @@ function Navbar() {
           </div>
 
           {/* Navbar Components */}
+
           <div className="hidden md:flex items-center space-x-4">
             <Button variant="ghost" className="flex items-center gap-2" asChild>
               <Link href="/plants">
-                <Sprout className="w-4 h-4 " />
+                <Sprout className="w-4 h-4" />
                 <span className="hidden lg:inline">Plants</span>
               </Link>
             </Button>
 
             <Button variant="ghost" className="flex items-center gap-2" asChild>
               <Link href="/">
-                <HomeIcon className="w-4 h-4 " />
+                <HomeIcon className="w-4 h-4" />
                 <span className="hidden lg:inline">Home</span>
               </Link>
             </Button>
+
             <ModeToggle />
+
+            {user ? (
+              <>
+                {/* SignOut Button */}
+                <Button variant="ghost" className="flex items-center gap-2" asChild>
+                  <Link href={app.signOut}>
+                    <LogOut className="w-4 h-4" />
+                    <span className="hidden lg:inline">Sign Out</span>
+                  </Link>
+                </Button>
+
+                <UserButton />
+              </>
+            ) : (
+              <>
+                {/* SignIn Button*/}
+                <Button variant="ghost" className="flex items-center gap-2" asChild>
+                  <Link href={app.signIn}>
+                    <LogIn className="w-4 h-4" />
+                    <span className="hidden lg:inline">Sign In</span>
+                  </Link>
+                </Button>
+              </>
+            )}
           </div>
         </div>
       </div>
