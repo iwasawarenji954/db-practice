766d6fe071e6f00faa6d2f0c9fadc58c654ef7f5
iwasawarenji954 <iwasawarenji954@gmail.com>
2025-05-26 09:17:17 +0900

    update: Navbar, shadcn, lucide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-   <h1>Hello World</h1>
+    <Button>Click me!</Button>
   );
 }
