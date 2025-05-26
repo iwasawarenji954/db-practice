58a583896dbe0c555249cce2c2c9a2b4472fd5e0
iwasawarenji954 <iwasawarenji954@gmail.com>
2025-05-26 09:48:40 +0900

    update: Dark Light の切り替え

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { HomeIcon, Sprout } from "lucide-react";
+import { ModeToggle } from "./ModeToggle";
 
 function Navbar() {
   return (
-    <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur-xl supports-[backdrop-filter]:bg-background/60 z-50">
+    <nav className="sticky top-0 h-12 w-full border-b bg-background/95 backdrop-blur-xl supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
-        <div className="flex items-center h-6 justify-between">
+        <div className="flex items-center h-12 justify-between">
           {/* Logo */}
           <div className="flex items-center">
             <Link
@@ -32,6 +33,7 @@ function Navbar() {
                 <span className="hidden lg:inline">Home</span>
               </Link>
             </Button>
+            <ModeToggle />
           </div>
         </div>
       </div>
